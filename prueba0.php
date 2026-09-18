2445f1fb8b95757ec00098bca2a11ec1c2a6650e
Alejandro Gadea <alexga22604@gmail.com>
2026-09-18 10:29:47 +0200

    Primer commit prueba

---
+++ prueba0.php
@@ -0,0 +1,17 @@
+<?php
+
+echo "Hello World!<br>";
+
+echo <<<END
+En un lugar de la Mancha,
+de cuyo nombre no quiero acordarme...
+END;
+
+echo "El número es 3";
+
+$cosa = 38;
+if ($cosa < 0) {
+    echo "sf";
+} else {
+    echo "LOOOL";
+}
