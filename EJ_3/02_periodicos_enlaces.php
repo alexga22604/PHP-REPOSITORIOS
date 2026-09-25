eea31a72042553af40a081461628fd022f4d1029
Alejandro Gadea <alexga22604@gmail.com>
2026-09-25 10:48:04 +0200

    ej3_02

---
+++ EJ_3/02_periodicos_enlaces.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<!--2.- Crear un array que almacene 5 cadenas con 
+    el nombre de periódicos y sus enlaces para acceder.
+     El array será asociativo con el nombre del 
+     periódico como clave y su URL como valor. 
+     Mostrar un lista html con cinco hiperenlaces a la URL de los diarios-->
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EJ3_02</title>
+
+    <?php
+
+    $medios = [
+        "El Pais" => "https://www.elpais.com",
+        "El Mundo" => "https://www.elmundo.es",
+        "ABC" => "https://www.abc.es",
+        "La Vanguardia" => "https://www.lavanguardia.com",
+        "El Confidencial" => "https://www.elconfidencial.com"
+    ];
+
+
+    function mostrarMedios($array_medios)
+    {
+        foreach ($array_medios as $nombre => $url) {
+            echo "<li><a href='$url'>$nombre</a></li>";
+        }
+    }
+    ?>
+
+
+</head>
+
+<body>
+    <ul>
+        <?php
+        mostrarMedios($medios);
+        ?>
+    </ul>
+</body>
+
+</html>
